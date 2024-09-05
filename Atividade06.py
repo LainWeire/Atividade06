dd742e3cdfc72bd139c73a2975b104c5190377c4
LainWeire <lucasfrota3000@gmail.com>
2024-09-05 10:13:44 -0300

    uff

---
+++ Atividade06.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+n1 = input("Seu primeiro nome: ")
+n2 = input("Seu segundo nome: ")
+print (n1 +" "+ n2)
+
